93ac97d1934960243af7c8897158a411f2eb19b3
Michael Johnson <33908025+Zy0d0x0@users.noreply.github.com>
2021-05-31 14:00:38 +0100

    Ardunio code for USB Keyboard commands
    This is a ardunio sketch to tell the radio what keyboard buttons have been pressed when the wheel is moved or clicked.

---
+++ Wheel.ino
@@ -0,0 +1,108 @@
+// Rotary Encoder Inputs
+#define CLK 2
+#define DT 3
+#define SW 4
+
+ uint8_t buf[8] = {
+  0
+}; 
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir ="";
+unsigned long lastButtonPress = 0;
+
+int upLeft = 79;
+int downRight = 80;
+int buttonClicked = 0;
+
+void setup() {
+  
+  // Set encoder pins as inputs
+  pinMode(CLK,INPUT);
+  pinMode(DT,INPUT);
+  pinMode(SW, INPUT_PULLUP);
+
+  // Setup Serial Monitor
+  Serial.begin(9600);
+
+  // Read the initial state of CLK
+  lastStateCLK = digitalRead(CLK);
+}
+
+void loop() {
+  
+  // Read the current state of CLK
+  currentStateCLK = digitalRead(CLK);
+
+  // If last and current state of CLK are different, then pulse occurred
+  // React to only 1 state change to avoid double count
+  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+
+    // If the DT state is different than the CLK state then
+    // the encoder is rotating CCW so decrement
+    if (digitalRead(DT) != currentStateCLK) {
+      counter --;
+      currentDir ="CCW";
+      buf[2] = upLeft; 
+      Serial.write(buf, 8);
+      releaseKey();
+    } else {
+      // Encoder is rotating CW so increment
+      counter ++;
+      currentDir ="CW";
+      buf[2] = downRight; 
+      Serial.write(buf, 8);
+      releaseKey();
+    }
+
+  }
+
+  // Remember last CLK state
+  lastStateCLK = currentStateCLK;
+
+  // Read the button state
+  int btnState = digitalRead(SW);
+
+  //If we detect LOW signal, button is pressed
+  if (btnState == LOW) {
+    //if 50ms have passed since last LOW pulse, it means that the
+    //button has been pressed, released and pressed again
+    if (millis() - lastButtonPress > 50 ) {
+
+      if (buttonClicked == 0) {
+        upLeft = 82;
+        downRight = 81;
+        buttonClicked = 1;
+  
+      } else {
+        upLeft = 79;
+        downRight = 80;
+        buttonClicked = 0;
+
+      }
+      
+    } else {
+      buf[2] = 40;    // HID: ENTER key
+      Serial.write(buf, 8);
+      releaseKey();
+    }
+
+    // Remember last button press event
+    lastButtonPress = millis();
+ 
+  
+    
+  }
+
+  // Put in a slight delay to help debounce the reading
+  delay(1);
+}
+
+void releaseKey()
+{
+  buf[0] = 0;
+  buf[2] = 0;
+  Serial.write(buf, 8); // Release key
+}
